1c92f38c97e4971d2ac549c1fae20d671a2e4a61
Kasper Skytte Andersen <knaldhat@gmail.com>
2019-09-02 12:02:05 +0200

    upped version for previous commits

--- autotax.sh
+++ autotax.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="1.2.7"
+VERSION="1.3.0"
 ##### requirements (tested with) #####
 # SILVA database files (both SSURef and typestrains) in arb format (use latest)
 # awk+grep+cat (included in most linux distributions)
