5d9ceb06be3247d75d20fdbdcee1c47d13bc8178
Kapper <knaldhat@gmail.com>
2020-03-19 15:58:45 +0100

    add script to run tests

--- runtests.sh
+++ runtests.sh
@@ -4,4 +4,4 @@ then
   echo "Error: tests must be run from the root of the AutoTax repository, not the test/ subfolder"
   exit 1
 fi
-sudo docker run -it -v "$(pwd):/autotax" autotax:1.1 bats -j 32 /autotax/test
+sudo docker run --rm -it -v "$(pwd):/autotax" autotax:1.1 bats /autotax/test
