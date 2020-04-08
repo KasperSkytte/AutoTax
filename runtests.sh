fb8e61c1237ec0889cd0db41a9ded351bac18e81
Kapper <knaldhat@gmail.com>
2020-04-08 14:42:58 +0200

    print time elapsed of test

--- runtests.sh
+++ runtests.sh
@@ -5,3 +5,5 @@ then
   exit 1
 fi
 sudo docker run --rm -it -v "$(pwd):/autotax" kasperskytte/autotax:latest bats /autotax/test
+duration=$(printf '%02dh:%02dm:%02ds\n' $(($SECONDS/3600)) $(($SECONDS%3600/60)) $(($SECONDS%60)))
+echo "Done in: $duration!"
