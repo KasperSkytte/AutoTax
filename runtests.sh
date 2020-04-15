3a34554b8a6b4380253f457aa65bd85ad05adba1
Kapper <knaldhat@gmail.com>
2020-04-15 14:59:55 +0200

    moved Dockerfile back again to avoid huge build context + will-cite fix

--- runtests.sh
+++ runtests.sh
@@ -4,7 +4,7 @@ then
   echo "Error: tests must be run from the root of the AutoTax git repository, not the test/ subfolder"
   exit 1
 fi
-sudo docker build -t kasperskytte:autotax:latest .
+sudo docker build -t kasperskytte/autotax:latest docker/
 (sudo docker run --rm -it -v "$(pwd):/autotax" kasperskytte/autotax:latest bats -t -j $((`nproc`-2)) /autotax/test) |& tee test_result.log
 duration=$(printf '%02dh:%02dm:%02ds\n' $(($SECONDS/3600)) $(($SECONDS%3600/60)) $(($SECONDS%60)))
 echo "Done in: $duration!"
